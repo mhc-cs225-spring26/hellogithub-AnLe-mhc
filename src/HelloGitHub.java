de19fc0d33b3c359c866a8830580f13be7309122
AnLe-mhc <le222t@mtholyoke.edu>
2026-02-06 12:41:01 -0500

    2nd commit of hello world

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,7 +3,10 @@ public class HelloGitHub {
 
     public static void main(String[] args) {
         // TODO Write your code here
-    	System.out.print("Hello, world");
+    	System.out.println("Hello, world");
+    	System.out.println("Xin chao");
+    	
+    	System.out.print("Chuc mung");
     }
 
 }
